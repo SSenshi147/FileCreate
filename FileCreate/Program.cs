172c55bb3871d3a7d4e334150fb3d2d11caefc0c
SSenshi147 <vojtko.marton@gmail.com>
2024-04-20 17:49:59 +0200

    Add project files.

---
+++ FileCreate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FileCreate;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        const string path = "out.txt";
+        if (!File.Exists(path))
+        {
+            // add the using here, so it won't lock the file after creation
+            using var _ =  File.Create(path);
+        }
+
+        File.WriteAllText(path, "test");
+        var content = File.ReadAllText(path);
+
+        Console.WriteLine(content);
+    }
+}
